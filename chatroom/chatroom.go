446016a00338df9ada326d26e8738f70e2a4f5ea
Yuki Ito <acomagu@gmail.com>
2017-06-10 14:55:07 +0900

    [Modify] remove methods

--- chatroom/chatroom.go
+++ chatroom/chatroom.go
@@ -12,42 +12,6 @@ type Room struct {
 	Out chan interface{}
 }
 
-// Flush inputs the value to be passed to Topic.
-func (cr Chatroom) Flush(v interface{}) {
-	cr.In <- v
-}
-
-// WaitSentMsg waits and returns the value passed to all of Room#Send. Used to send messages to user (through chat service).
-func (cr Chatroom) WaitSentMsg() interface{} {
-	text := <-cr.Out
-	return text
-}
-
-// WaitSentTextMsg waits and returns the string value. It ignores the others.
-func (cr Chatroom) WaitSentTextMsg() string {
-	for {
-		if str, ok := cr.WaitSentMsg().(string); ok {
-			return str
-		}
-	}
-}
-
-func (room Room) WaitMsg() interface{} {
-	return <-room.In
-}
-
-func (room Room) WaitTextMsg() string {
-	for {
-		if str, ok := (<-room.In).(string); ok {
-			return str
-		}
-	}
-}
-
-func (room Room) Send(v interface{}) {
-	room.Out <- v
-}
-
 // New creates and initialize a Chatroom. This also starts a go-routine to pass messages to Topics.
 func New(topics []Topic) Chatroom {
 	room := Room{
